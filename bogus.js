c84c470762a31719094bb6b71bf84cbea0211636
Tao Xu <tao2019@fb.com>
2019-05-20 14:15:33 -0700

    add src

---
+++ bogus.js
@@ -0,0 +1,145 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <title>Bogusland: Land of the Bogus</title>
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js?r=stable');
+  fbq('init', '2019183905013969');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=2019183905013969&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+  
+  <!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js?r=stable');
+  fbq('init', '1666949093426012');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1666949093426012&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+  
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js?r=stable');
+  fbq('init', '311052276305757');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=311052276305757&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
+</head>
+
+
+<body>
+  <h1>Welcome to Bogusland!</h1>
+  <div style="margin-bottom: 50px;">
+    <p>Where the land is bogus</p>
+  </div>
+  <div style="margin-bottom: 100px;">
+    <a class="btn" href="/" style="margin: 15px; border: solid 1px; padding: 20px;">Contact</a>
+    <a class="btn" href="/" style="margin: 15px; border: solid 1px; padding: 20px;">Checkout</a>
+    <a class="btn" href="/" style="margin: 15px; border: solid 1px; padding: 20px;">Add to Wishlist</a>
+    <a class="btn" href="/" style="margin: 15px; border: solid 1px; padding: 20px;">Search</a>
+  </div>
+  <div>
+    <a class="btn" href="/" style="margin: 15px; border: solid 1px; padding: 20px;" onclick="window.fbq('track', 'AddToCart')">Bogus One</a>
+    <a class="btn" href="/" style="margin: 15px; border: solid 1px; padding: 20px;" onclick="window.fbq('track', 'Lead')">Bogus Two</a>
+    <a class="btn" href="/" style="margin: 15px; border: solid 1px; padding: 20px;" onclick="window.fbq('track', 'Purchase')">Bogus Three</a>
+    <a class="btn" href="/" style="margin: 15px; border: solid 1px; padding: 20px;" onclick="window.fbq('track', 'Search')">Bogus Four</a>
+  </div>
+  <div>
+    <div style="margin-top: 200px;">
+      <a href="/">Anchor 1</a>
+      <a href="/">Anchor 2</a>
+      <a href="/"><img src="https://placekitten.com/100/100" /></a>
+    </div>
+  </div>
+  <div>
+    <div style="margin-top: 300px;" itemscope itemtype="http://schema.org/Product">
+      <h2>Bogus product</h2>
+      <meta itemprop="name" content="Bogus product">
+      <div itemscope itemprop="offers" itemtype="http://schema.org/Offer">
+        <meta itemprop="priceCurrency" content="USD">
+        <span itemprop="price" content="10.0">$10</span>
+      </div>
+      <img src="https://placekitten.com/400/200" />
+      <a href="/" style="margin: 15px; border: solid 1px; padding: 20px;">Add to Cart</a>
+    </div>
+    <div style="margin-top: 300px;" itemscope itemtype="http://schema.org/Product">
+      <h2>Bogus product 2</h2>
+      <meta itemprop="name" content="Bogus product 2">
+      <div itemscope itemprop="offers" itemtype="http://schema.org/Offer">
+        <meta itemprop="priceCurrency" content="USD">
+        <span itemprop="price" content="20.0">$20</span>
+      </div>
+      <a href="image_anchor.html">
+        <img src="https://placekitten.com/400/200" />
+      </a>
+      <a class="btn" href="/" style="margin: 15px; border: solid 1px; padding: 20px;">Add to Cart</a>
+    </div>
+    <div style="margin-top: 300px;" itemscope itemtype="http://schema.org/Product">
+      <h2>Bogus product 3</h2>
+      <meta itemprop="name" content="Bogus product 3">
+      <div itemscope itemprop="offers" itemtype="http://schema.org/Offer">
+        <meta itemprop="priceCurrency" content="USD">
+        <span itemprop="price" content="300.0">$300</span>
+      </div>
+      <img src="https://placekitten.com/400/200" />
+      <a class="btn" href="/" style="margin: 15px; border: solid 1px; padding: 20px;">Add to Cart</a>
+    </div>
+    <div style="margin-top: 300px;" itemscope itemtype="http://schema.org/Product">
+      <h2>Bogus product 4</h2>
+      <meta itemprop="name" content="Bogus product 4">
+      <div itemscope itemprop="offers" itemtype="http://schema.org/Offer">
+        <meta itemprop="priceCurrency" content="USD">
+        <span itemprop="price" content="4000.0">$4000</span>
+      </div>
+      <img src="https://placekitten.com/400/200" />
+      <a class="btn" href="/" style="margin: 15px; border: solid 1px; padding: 20px;">Add to Cart</a>
+    </div>
+    <div style="margin-top: 300px;" itemscope itemtype="http://schema.org/Product">
+      <h2>Bogus product 5</h2>
+      <meta itemprop="name" content="Bogus product 5">
+      <div itemscope itemprop="offers" itemtype="http://schema.org/Offer">
+        <meta itemprop="priceCurrency" content="USD">
+        <span itemprop="price" content="50000.0">$50000</span>
+      </div>
+      <img src="https://placekitten.com/400/200" />
+      <a class="btn" href="/" style="margin: 15px; border: solid 1px; padding: 20px;">Add to Cart</a>
+    </div>
+  </div>
+  <a href="mailto:fakename@fakedomain.com">Email link</a>
+  <a class="btn" href="/" style="margin-top: 50px; border: solid 1px; padding: 20px;">End of the world</a>
+</body>
+</html>
+
